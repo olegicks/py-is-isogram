14dc9bf66aca87464110ca01d60815f42a8deff0
Oleh <sholeg2005@gmail.com>
2024-11-27 00:40:42 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,24 +2,24 @@ from app.main import is_isogram
 
 
 def test_different_letters() -> None:
-    isogram = 'playgrounds'
+    isogram = "playgrounds"
     result = is_isogram(isogram)
-    assert result == True
+    assert result is True
 
 
 def test_big_and_small_letter() -> None:
-    isogram = 'Adam'
+    isogram = "Adam"
     result = is_isogram(isogram)
-    assert result == False
+    assert result is False
 
 
 def test_empty_string() -> None:
-    isogram = ''
+    isogram = ""
     result = is_isogram(isogram)
-    assert result == True
+    assert result is True
 
 
 def test_small_different() -> None:
-    isogram = 'look'
+    isogram = "look"
     result = is_isogram(isogram)
-    assert result == False
+    assert result is False
